b92576665446f8447e31e10b1d38291eeb4d9d8c
ssssssyyyyyy <yuto_shioda@yahoo.co,jp>
2019-10-22 02:32:17 +0900

    ver.1.7

--- html/003_officeial/js/script.js
+++ html/003_officeial/js/script.js
@@ -7,11 +7,3 @@ $(".slider").slick({
 	autoplaySpeed: 2000,
 	pauseOnHover: false
 });
-
-
-$(window).on("load resize", function(){
-	var wH = $(window).height();
-	var mH = $("#main").height();
-	var padd = (wH - mH)/2;
-	$("#main").css("padding",padd+"px 0");
-});
