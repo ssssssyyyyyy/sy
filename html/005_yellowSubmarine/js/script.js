e869cf66d5855e87d00f514a50ed248b31730855
ssssssyyyyyy <yuto_shioda@yahoo.co,jp>
2019-11-06 17:11:16 +0900

    ver1.0.7

--- html/005_yellowSubmarine/js/script.js
+++ html/005_yellowSubmarine/js/script.js
@@ -13,10 +13,8 @@
 //		}else{
 //			var num = maxI-(maxI-1)+i;
 //			$(".yellowSubmarine_text_"+num).fadeOut();
-//			console.log("増やす前"+i);
 //			i++;
 //			i++;
-//			console.log("増やした後"+i);
 //			return i;			
 //		}
 //	}	
@@ -33,25 +31,23 @@ $(function() {
 
 	function textEffect(target,delay){
 		$(target).textillate({
-		selector: target, //要素の指定
-		loop: true, //アニメーションの繰り返し
-		minDisplayTime: 3000, //アニメーションの間隔
-		autoStart: true, //アニメーションの自動スタート
-		inEffects: ["bounceInUp"], //エフェクト開始時のアニメーション
-		outEffects: ["bounceOutUp"],//エフェクト終了時のアニメーション
+		selector: target, 
+		loop: true, 
+		minDisplayTime: 3000, 
+		autoStart: true, 
+		inEffects: ["bounceInUp"], 
+		outEffects: ["bounceOutUp"],
 
-			//アニメーション開始時の設定
 	 	in: {
-	 	  effect: "bounceInUp", //エフェクトの指定
-	 	  delayScale: 0.5, //各文字に適用される遅延係数
-	 	  delay: 50, //文字ごとの遅延時間
-	 	  sync: false, //アニメーションをすべての文字に同時に適用
-	 	  shuffle: false, //ランダム表示
-	 	  reverse: false, //エフェクトの逆再生(sync:trueとの併用は不可)
-	 	  callback: function () {} //コールバック関数
+	 	  effect: "bounceInUp", 
+	 	  delayScale: 0.5, 
+	 	  delay: 50, 
+	 	  sync: false, 
+	 	  shuffle: false, 
+	 	  reverse: false, 
+	 	  callback: function () {} 
 	 	},
 
-		//アニメーション終了時の設定
 		out: {
 		  effect: "bounceOutUp",
 		  delayScale: 1.5,
@@ -63,7 +59,7 @@ $(function() {
 		},
 		callback: function () {},
 		type: "char",
-		initialDelay: delay //アニメーション開始までの遅延
+		initialDelay: delay 
 		});
 	}
 })
